af28079601d6b521271c3f78e8aa21586b07b135
Harish <purple.evander@gmail.com>
2024-11-06 17:41:19 +0530

    server port modifications

---
+++ server/ecosystem.config.js
@@ -0,0 +1,14 @@
+const { name } = require("ejs");
+
+module.exports={
+    apps:[
+        {
+            name: "project-management",
+            script: "npm",
+            args: "run dev",
+            env: {
+                NODE_ENV: "development",
+            },
+        },
+    ],
+};
